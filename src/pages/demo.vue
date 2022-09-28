02c8f115906541244766bee54a508eaf2fd78f27
Vita <kklevita92@gmail.com>
2022-09-28 20:17:25 +0300

    starting commit

---
+++ src/pages/demo.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-page class="flex flex-center"> </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "IndexPage",
+  data: () => ({
+    scene: scene,
+    camera: camera,
+    controls: [],
+    renderer: renderer,
+    light: light,
+    cube: cube,
+    axes: axes,
+    speed: 0.01,
+  }),
+});
+</script>
---
+++ src/router/routes.js
@@ -0,0 +1,15 @@
+const routes = [
+  {
+    path: "/",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [{ path: "", component: () => import("pages/IndexPage.vue") }],
+  },
+
+  // Always leave this as last one,
+  {
+    path: "/:catchAll(.*)*",
+    component: () => import("pages/ErrorNotFound.vue"),
+  },
+];
+
+export default routes;
